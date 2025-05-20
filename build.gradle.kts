f12db24114de50680abf18bf02e2d96a008920b1
Canberk Ozcelik <ext_canberk.ozcelik@spark.net>
2025-05-20 12:36:53 +0200

    create github actions main.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     kotlin("jvm") version "1.9.22"
     kotlin("plugin.spring") version "1.9.22"
     kotlin("plugin.jpa") version "1.9.22"
+    id("org.jlleitschuh.gradle.ktlint") version "11.5.0"
 }
 
 group = "com.f1champions"
