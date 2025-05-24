33377b79c425534308f29ce68484df8e7d8514c4
Canberk Ozcelik <canberko@hexaware.com>
2025-05-24 12:27:23 +0200

    fix: linter issues

--- src/test/kotlin/com/f1champions/controller/F1ControllerTest.kt
+++ src/test/kotlin/com/f1champions/controller/F1ControllerTest.kt
@@ -19,11 +19,7 @@ import org.springframework.test.web.servlet.MvcResult
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.asyncDispatch
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.header
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.request
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
 @WebMvcTest(F1Controller::class)
 class F1ControllerTest {
@@ -277,4 +273,3 @@ class F1ControllerTest {
             .andExpect(jsonPath("$.message").value("Received invalid data from Formula 1 data service"))
     }
 }
- 
