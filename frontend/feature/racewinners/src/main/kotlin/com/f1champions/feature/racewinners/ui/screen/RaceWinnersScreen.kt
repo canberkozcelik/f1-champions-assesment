ffe5ac9a0c7c5586dcf5e87c9a98f1bcda879df7
Canberk Ozcelik <canberko@hexaware.com>
2025-05-30 16:59:04 +0200

    feat(frontend): fix additional imports

--- frontend/feature/racewinners/src/main/kotlin/com/f1champions/feature/racewinners/ui/screen/RaceWinnersScreen.kt
+++ frontend/feature/racewinners/src/main/kotlin/com/f1champions/feature/racewinners/ui/screen/RaceWinnersScreen.kt
@@ -15,7 +15,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -27,7 +26,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -97,16 +95,6 @@ fun RaceWinnersScreen(
                 }
             }
         }
-    }
-}
-
-@Composable
-private fun LoadingIndicator() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        CircularProgressIndicator()
     }
 }
 
